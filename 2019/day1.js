155f12d52327346b43e88242a6f5697bb82ea9e0
jayanth-14 <jayanthpetchetii@gmail.com>
2025-12-03 11:50:44 +0530

    completed 2019/day1

--- 2019/day1.js
+++ 2019/day1.js
@@ -1,1 +1,13 @@
+const divideBy3 = number => number / 3;
+
+const subtract2 = number => number - 2;
+
+export const fuelRequired = mass => {
+  const fuel = subtract2(Math.floor(divideBy3(mass)));
+  return fuel > 0 ? fuelRequired(fuel) : 0;
+}
+
+const fuelRequirements = starMasses => starMasses.reduce((sum, mass) => sum + fuelRequired(mass), 0);
+
+const total = fuelRequirements(data.split("\n").filter(x => x !== "").map(x => parseInt(x)));
 
