7dfb6ef6e53d7c92f33e246635f655f392b507f7
jayanth-14 <jayanthpetchetii@gmail.com>
2025-12-04 09:17:26 +0530

    completed third day

--- 2019/day3.js
+++ 2019/day3.js
@@ -18,7 +18,6 @@ const performInstruction = (instruction, location) => {
 	const [direction, steps] = parse(instruction);
 	const locations = [];
 	let lastLocation = location;
-	locations.push(location);
 	for(let i = 0; i < steps; i++) {
 		lastLocation = move(direction,lastLocation)
 		locations.push(lastLocation);
@@ -49,4 +48,39 @@ const intersecting = (points1, points2) => {
 	return intersects;
 }
 
-const areSamePoints = (point1, point2) => point1.toString() === point2.toString();
+const areSamePoints = (point1, point2) => point1[0] === point2[0] && point1[1] === point2[1];
+
+const getDistance = ([x1, y1], [x2, y2]) => Math.abs(x1 - x2) + Math.abs(y1 - y2);
+
+const mapDistances = (points, fromPoint = [0,0]) => {
+	const map = {};
+	for (const point of points) {
+		const distance = getDistance(point, fromPoint);
+		map[distance] = point;
+	}
+	return map;
+}
+
+const shortestPoint = (distanceMap) => {
+  const distances = Object.keys(distanceMap).map(distance => parseInt(distance));
+  const shortest = Math.min(...distances);
+  return distanceMap[shortest];
+}
+
+const findSteps = (point, allPoints) => allPoints.findIndex(cp => cp[0] === point[0] && cp[1] === point[1]) + 1;
+
+const findAllSteps = (intersects, firstWire, secondWire) => {
+	const steps = [];
+	for (const inter of intersects) {
+		const firstWireSteps = findSteps(inter, firstWire);
+		const secondWireSteps = findSteps(inter, secondWire);
+		const totalSteps = add(firstWireSteps, secondWireSteps);
+    steps.push(totalSteps);
+	}
+	return steps;
+}
+
+const shortestSteps = (intersects, firstWire, secondWire) => {
+	const allSteps = findAllSteps(intersects, firstWire, secondWire);
+	return Math.min(...allSteps);
+}
