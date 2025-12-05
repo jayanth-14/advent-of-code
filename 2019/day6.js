6a59eaa35610f1cd8b50d9a9b23f6e84da6c3c56
jayanth-14 <jayanthpetchetii@gmail.com>
2025-12-05 10:54:01 +0530

    written basic process

--- 2019/day6.js
+++ 2019/day6.js
@@ -5,3 +5,15 @@
 
 // Example: if x)y y)z => that means z is directly y and indirectly orbiting around x. here it is 2 and y is also orbiting x then it is 1 ===> total 3 orbits.
 
+// Do I need to question?
+// Yes, I need to ask How I am going to count a single object's orbit count.
+
+// How am I going to count a single object's orbit count.
+// I will see what current element is orbiting around.
+// Then increase the count.
+// => Then find what the parent is orbiting around.
+// => if it is orbiting around something, find the what it is orbiting.
+// => increase the count.
+// => repeat till we find something which don't revolve around anything.
+
+
