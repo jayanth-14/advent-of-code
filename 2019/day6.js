a1fa3e5ad9f3c4f99833589a97277898c9648804
jayanth-14 <jayanthpetchetii@gmail.com>
2025-12-05 10:47:29 +0530

    answered first question for day6

---
+++ 2019/day6.js
@@ -0,0 +1,7 @@
+// Do I know what I should do? 
+// I need to find total how many direct and indirect orbits exists.
+// Any object on the space is orbiting around another object. Except `COM`.
+// So I will have all the objects and what are they orbiting around. And then check what the orbited object is orbiting.
+
+// Example: if x)y y)z => that means z is directly y and indirectly orbiting around x. here it is 2 and y is also orbiting x then it is 1 ===> total 3 orbits.
+
